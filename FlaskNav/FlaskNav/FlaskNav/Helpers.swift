58b759b8c5168299f585b3b311fbac2165357eb9
hassan uriostegui <hassanvfx@gmail.com>
2018-09-18 20:22:03 -0700

    basic setup with flask

---
+++ FlaskNav/FlaskNav/FlaskNav/Helpers.swift
@@ -0,0 +1,22 @@
+//
+//  FlaskMavManifest.swift
+//  FlaskNav
+//
+//  Created by hassan uriostegui on 9/15/18.
+//  Copyright © 2018 eonflux. All rights reserved.
+//
+
+import UIKit
+
+
+public typealias NavConstructor = () -> UIViewController
+public typealias RoutingMap = [String:NavConstructor]
+
+extension FlaskNav{
+    
+    
+    
+    
+}
+
+
