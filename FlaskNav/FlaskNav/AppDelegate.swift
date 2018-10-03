bc7646090e18fa6ab008a2a7c21f80d7ddc89200
hassan uriostegui <hassanvfx@gmail.com>
2018-10-03 16:56:55 -0700

    all tests working

--- FlaskNav/FlaskNav/AppDelegate.swift
+++ FlaskNav/FlaskNav/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         Services.router.setup(withWindow: window!)
         
-        testTabAnimation()
+        testTransaction()
 //        testModal()
 //        testTransaction()
 //        testAPI()
