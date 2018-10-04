71e0f7b3dec0beebd7b05b6d4f4ec8a140e5a714
hassan uriostegui <hassanvfx@gmail.com>
2018-10-03 17:20:01 -0700

    updated API to pass objects instead of json strings for aniamtors

--- FlaskNav/FlaskNav/FlaskNavigation/Animations/Presentation/NavPresentationController.swift
+++ FlaskNav/FlaskNav/FlaskNavigation/Animations/Presentation/NavPresentationController.swift
@@ -8,21 +8,21 @@
 
 import UIKit
 
-final class NavPresentationClass:  UIPresentationController {
+public final class NavPresentationClass:  UIPresentationController {
 //    var touchForwardingView:TouchForwardingView!
     
-    override var frameOfPresentedViewInContainerView:CGRect {
+    override public var frameOfPresentedViewInContainerView:CGRect {
         return containerView!.bounds
         
 //        let height: CGFloat = 200
 //        return CGRect(x: 0, y: containerView!.bounds.height - height, width: containerView!.bounds.width, height: height)
     }
     
-    override func containerViewWillLayoutSubviews() {
+    override public func containerViewWillLayoutSubviews() {
         presentedView?.frame = frameOfPresentedViewInContainerView
     }
     
-    override func presentationTransitionWillBegin() {
+    override public func presentationTransitionWillBegin() {
         super.presentationTransitionWillBegin()
 //        touchForwardingView = TouchForwardingView(frame: containerView!.bounds)
 //        touchForwardingView.forwardingViews = [presentingViewController.view];
--- FlaskNav/FlaskNav/FlaskNavigation/Nav/API/Nav+Stack.swift
+++ FlaskNav/FlaskNav/FlaskNavigation/Nav/API/Nav+Stack.swift
@@ -39,7 +39,7 @@ extension FlaskNav{
 
 extension FlaskNav: NavStackAPI{
     
-    func push(layer:String, batched:Bool = false, controller:String , resourceId:String?, info:Any? = nil, animator: String? = nil, presentation: String? = nil, callback: NavContextCallback?) {
+    func push(layer:String, batched:Bool = false, controller:String , resourceId:String?, info:Any? = nil, animator: NavAnimatorClass? = nil, presentation: NavPresentationClass? = nil, callback: NavContextCallback?) {
         queueIntent(batched:batched) { [weak self] in
             let context = NavContext.manager.context(layer:layer, controller: controller, resourceId: resourceId, info: info, callback)
             self?.setActive(layer:layer)
