9a7b915705682e69dbd79878b04736757e4324ae
hassan uriostegui <hassanvfx@gmail.com>
2018-10-13 18:55:33 -0700

    succcesful refactor

--- FlaskNav/FlaskNav/FlaskNavigation/Animations/Animators/NavAnimatorClass.swift
+++ FlaskNav/FlaskNav/FlaskNavigation/Animations/Animators/NavAnimatorClass.swift
@@ -24,6 +24,7 @@ open class NavAnimatorClass: NSObject {
     public private(set) var type:NavAnimatorClassType = .Show
     public private(set) var controller:NavAnimatorControllerType = .ViewController
     public var _duration = 0.4
+    var viewAnimator:UIViewPropertyAnimator?
     
     //MARK: INTERACTOR
     public var onInteractionRequest:NavAnimatorInteraction?
@@ -31,10 +32,10 @@ open class NavAnimatorClass: NSObject {
     public private(set) var _interactionController:UIPercentDrivenInteractiveTransition? = nil
     
    //MARK: SUBCLASS OVERRIDES
-    open func present(controller:UIViewController,from fromController:UIViewController,in containerView:UIView, withContext context:UIViewControllerContextTransitioning){
+    open func present(controller:UIViewController,from fromController:UIViewController,in containerView:UIView, withContext context:UIViewControllerContextTransitioning)->UIViewPropertyAnimator?{
         assert(false,"use a subclass instead")
     }
-    open func dismiss(controller:UIViewController,to toController:UIViewController,in containerView:UIView, withContext context:UIViewControllerContextTransitioning){
+    open func dismiss(controller:UIViewController,to toController:UIViewController,in containerView:UIView, withContext context:UIViewControllerContextTransitioning)->UIViewPropertyAnimator?{
         assert(false,"use a subclass instead")
     }
  
@@ -52,6 +53,8 @@ extension NavAnimatorClass:UIViewControllerAnimatedTransitioning{
     
     public func animationEnded(_ transitionCompleted: Bool) {
         
+        viewAnimator = nil
+        
         if !transitionCompleted {
             onInteractionCanceled?(self)
         }
@@ -61,31 +64,34 @@ extension NavAnimatorClass:UIViewControllerAnimatedTransitioning{
     public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return _duration
     }
-    
-    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+  
+    public func interruptibleAnimator(using transitionContext: UIViewControllerContextTransitioning) -> UIViewImplicitlyAnimating {
         
-        guard
-            let toController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to),
-            let fromController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
-            else{
-                assert(false,"invalid layer")
-                return
+        if let viewAnimator = self.viewAnimator {
+            return viewAnimator
         }
         
-        
+        let toController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
+        let fromController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
         let container = transitionContext.containerView
         
         if type == .Show {
             container.addSubview(toController.view)
-            present(controller: toController, from: fromController, in: container, withContext: transitionContext)
+            viewAnimator = present(controller: toController, from: fromController, in: container, withContext: transitionContext)
         } else if type == .Hide{
             
             if controller == .Navigation {
                 container.insertSubview(toController.view, belowSubview: fromController.view)
             }
             
-            dismiss(controller: fromController, to: toController, in: container, withContext: transitionContext)
+            viewAnimator = dismiss(controller: fromController, to: toController, in: container, withContext: transitionContext)
         }
+        return viewAnimator!
+    }
+    
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        interruptibleAnimator(using: transitionContext).startAnimation()
     }
 }
 
